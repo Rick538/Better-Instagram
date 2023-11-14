771f1bab03e456af3f3f645e8194755ab6f21ea3
Rick538 <68424088+Rick538@users.noreply.github.com>
2023-11-14 18:45:50 +0100

    verze.9

--- instagram.py
+++ instagram.py
@@ -9,19 +9,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from secret import USERNAME, PASSWORD
 from selenium.webdriver.common.keys import Keys
+import requests
+from icecream import ic
 
 
  
 # Creating an instance of the Instaloader class
 def load_instagram():
-    """Create and save session of my instagram account"""
+    """Load data from instagram"""
     username = input("Enter your Instagram username: ")
     bot = instaloader.Instaloader()
     bot.load_session_from_file("karlito_podel9")
-    profile = instaloader.Profile.from_username(bot.context, username)
+    profile_insta = instaloader.Profile.from_username(bot.context, username)
     print("Instagram profile")
-    return profile
-
+    return profile_insta
 
 #Opening the google browser
 def Opening_Google():
@@ -32,10 +33,15 @@ def Opening_Google():
     driver = webdriver.Chrome(options)
     return driver
 
-def profile_data(profile):
-    """Takes data from instagram profile and save them into json.file"""
+def clear_post_data():
+    """Clear the json file named post_data"""
     with open('post_data.json', 'w', encoding='utf-8') as json_file:
         json_file.truncate()
+
+def profile_data():
+    """Takes data from instagram profile and save them into json.file"""
+    clear_post_data()
+    profile = load_instagram()
     print("Profile data")
     about_user = {
         "Username: ": profile.username,
@@ -47,43 +53,52 @@ def profile_data(profile):
     post_number = 0
     post_data_list = []
     post_data_list.append(about_user)
-    for post in profile.get_posts():
-        post_number += 1
-        if post_number <= 10:
-            time.sleep(5)
-            post_dict = {
-                "Post_id: ": post_number,
-                "Post date: ": str(post.date_local),
-                "Post caption: ": post.caption,
-                "Post likes count: ": post.likes,
-                "Post comments count: ": post.comments,
-                "Post location: ": post.location,
-                "Post hashtags: ": post.caption_hashtags
-            }
-            post_data_list.append(post_dict)
-        else:
-            with open('post_data.json', 'w', encoding='utf-8') as json_file:
-                json.dump(post_data_list, json_file, ensure_ascii=False,indent=3)
-            print_jason()
+    if profile.get_posts() == "":
+        with open('post_data.json', 'w', encoding='utf-8') as json_file:
+            json.dump(post_data_list, json_file, ensure_ascii=False,indent=3)
+        print_jason()
+    else:
+        for post in profile.get_posts():
+            post_number += 1
+            if post_number <= 10:
+                time.sleep(5)
+                post_dict = {
+                    "Post_id: ": post_number,
+                    "Post date: ": str(post.date_local),
+                    "Post caption: ": post.caption,
+                    "Post likes count: ": post.likes,
+                    "Post comments count: ": post.comments,
+                    "Post location: ": post.location,
+                    "Post hashtags: ": post.caption_hashtags
+                }
+                post_data_list.append(post_dict)
+            else:
+                with open('post_data.json', 'w', encoding='utf-8') as json_file:
+                    json.dump(post_data_list, json_file, ensure_ascii=False,indent=3)
+                print_jason()
 
 def print_jason():
-    """This will formate data in json.filento readable text for chatgpt"""
+    """This will formate data in json.file into readable text for chatgpt"""
     with open('post_data.json', 'r', encoding='utf-8') as json_file:
         data = json.load(json_file)
         formated = re.sub(r'[{}"\[\],]','', json.dumps(data, indent=3))
         return formated
     
     
-def Sending_prompt_to_chatgpt(driver):
+def Sending_prompt_to_chatgpt():
     """Send a data from instagram to the chatgpt for analytics of better performance"""
-    json_data = str(print_jason())
-    question = ("Tell me what should i change on my instagram profile to make it better and get more attention from people, use this data for analysis:"
-    "1) username: do you think that this username is good? or you think i should change it to make my profile look better" 
-    "2) bio: do you think that this bio is good and creative or at least if it can tell what person im? "
-    "3) hastags: are those hastags that i have on my post good or should i try add newer?"
-    "4) date: should i posted a more posts in short time or is this period good, like should i post every day or week"
-    "5) caption: is the text of the caption good? or i should change it into something more informative, like info about the author or the post"
-    "Please answer in the following questions and on the end of every question and add some space at the end, please do not write the question, but only the point and the first word in which it is found and the answer, because it would be helpful and more readable")
+    driver = Opening_Google()
+    data_for_chatgpt = str(print_jason())
+    question = """Tell me what should i change on my instagram profile to make it better and get more attention from people, use this data for analysis:
+    1) username: do you think that this username is good? or you think i should change it to make my profile look better"
+    2) bio: do you think that this bio is good and creative or at least if it can tell what person im? 
+    3) hastags: are those hastags that i have on my post good or should i try add newer?
+    4) date: should i posted a more posts in short time or is this period good, like should i post every day or week
+    5) caption: is the text of the caption good? or i should change it into something more informative, like info about the author or the post
+    Please answer in the following questions and on the end of every question, and add some space at the end, please do not write the question, but only the point and the first word in which it is found and the answer, because it would be helpful and more readable
+    """ + data_for_chatgpt
+    print(question)
+    time.sleep(10)
     driver.get("https://chat.openai.com")
     time.sleep(5)
     try: 
@@ -94,7 +109,7 @@ def Sending_prompt_to_chatgpt(driver):
     time.sleep(2)
     driver.find_element(By.ID,'prompt-textarea').send_keys(question)
     time.sleep(2)
-    lines = json_data.splitlines()
+    lines = data_for_chatgpt.splitlines()
     for line in lines:
         if line != '\n':
             driver.find_element(By.ID,'prompt-textarea').send_keys(line)
@@ -136,10 +151,8 @@ def login_into_chatgpt(USERNAME,PASSWORD,driver):
 
 def main():
     #Takes a list of data from instagram and write them into a json.file
-    data = load_instagram()
-    profile_data(data)
-    google = Opening_Google()
-    Sending_prompt_to_chatgpt(google)
+    profile_data()
+    Sending_prompt_to_chatgpt()
 
 main()
    
