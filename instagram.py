f069394268067a223ce0aedb900a17207bf98cb6
Rick538 <68424088+Rick538@users.noreply.github.com>
2023-11-15 23:20:28 +0100

    verze.10

--- instagram.py
+++ instagram.py
@@ -29,7 +29,7 @@ def Opening_Google():
     """Open the google browser with saved session"""
     options = Options()
     options.add_argument("user-data-dir=C:\selenium")
-    options.binary_location = r"C:\Program Files\Google\Chrome Beta\Application\chrome.exe"
+    options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
     driver = webdriver.Chrome(options)
     return driver
 
@@ -53,29 +53,24 @@ def profile_data():
     post_number = 0
     post_data_list = []
     post_data_list.append(about_user)
-    if profile.get_posts() == "":
-        with open('post_data.json', 'w', encoding='utf-8') as json_file:
-            json.dump(post_data_list, json_file, ensure_ascii=False,indent=3)
-        print_jason()
-    else:
-        for post in profile.get_posts():
-            post_number += 1
-            if post_number <= 10:
-                time.sleep(5)
-                post_dict = {
-                    "Post_id: ": post_number,
-                    "Post date: ": str(post.date_local),
-                    "Post caption: ": post.caption,
-                    "Post likes count: ": post.likes,
-                    "Post comments count: ": post.comments,
-                    "Post location: ": post.location,
-                    "Post hashtags: ": post.caption_hashtags
-                }
-                post_data_list.append(post_dict)
-            else:
-                with open('post_data.json', 'w', encoding='utf-8') as json_file:
-                    json.dump(post_data_list, json_file, ensure_ascii=False,indent=3)
-                print_jason()
+    for post in profile.get_posts():
+        post_number += 1
+        if post_number <= 10:
+            time.sleep(5)
+            post_dict = {
+                "Post_id: ": post_number,
+                "Post date: ": str(post.date_local),
+                "Post caption: ": post.caption,
+                "Post likes count: ": post.likes,
+                "Post comments count: ": post.comments,
+                "Post location: ": post.location,
+                "Post hashtags: ": post.caption_hashtags
+            }
+            post_data_list.append(post_dict)
+        else:
+            with open('post_data.json', 'w', encoding='utf-8') as json_file:
+                json.dump(post_data_list, json_file, ensure_ascii=False,indent=3)
+            print_jason()
 
 def print_jason():
     """This will formate data in json.file into readable text for chatgpt"""
@@ -83,8 +78,7 @@ def print_jason():
         data = json.load(json_file)
         formated = re.sub(r'[{}"\[\],]','', json.dumps(data, indent=3))
         return formated
-    
-    
+
 def Sending_prompt_to_chatgpt():
     """Send a data from instagram to the chatgpt for analytics of better performance"""
     driver = Opening_Google()
@@ -98,17 +92,13 @@ def Sending_prompt_to_chatgpt():
     Please answer in the following questions and on the end of every question, and add some space at the end, please do not write the question, but only the point and the first word in which it is found and the answer, because it would be helpful and more readable
     """ + data_for_chatgpt
     print(question)
-    time.sleep(10)
+    time.sleep(5)
     driver.get("https://chat.openai.com")
     time.sleep(5)
-    try: 
-        driver.find_element(By.ID,'prompt-textarea').click() 
-    except:
-        login_into_chatgpt(USERNAME,PASSWORD,driver)
     driver.find_element(By.ID,'prompt-textarea').click() 
-    time.sleep(2)
+    time.sleep(3)
     driver.find_element(By.ID,'prompt-textarea').send_keys(question)
-    time.sleep(2)
+    time.sleep(3)
     lines = data_for_chatgpt.splitlines()
     for line in lines:
         if line != '\n':
@@ -127,33 +117,10 @@ def Taking_response_data(driver):
     response.write(response_from_chatgpt)
     print(response.read())
 
-def login_into_chatgpt(USERNAME,PASSWORD,driver):
-        """This will login into your account in chatgpt if you dont have cookies, and makes you cookies so u dont have to write an email, password every time"""
-        login_button = driver.find_element(By.CSS_SELECTOR,'#__next > div.flex.min-h-full.w-screen.flex-col.sm\:supports-\[min-height\:100dvh\]\:min-h-\[100dvh\].md\:grid.md\:grid-cols-2.lg\:grid-cols-\[60\%_40\%\] > div.relative.flex.grow.flex-col.items-center.justify-between.bg-white.px-5.py-8.text-black.dark\:bg-black.dark\:text-white.sm\:rounded-t-\[30px\].md\:rounded-none.md\:px-6 > div.relative.flex.w-full.grow.flex-col.items-center.justify-center > div > div > button:nth-child(1)')
-        login_button.click()
-        time.sleep(3)
-        email_chatgpt = driver.find_element(By.CSS_SELECTOR,'#username')
-        time.sleep(3)
-        email_chatgpt.send_keys(USERNAME)
-        time.sleep(3)
-        button = driver.find_element(By.CSS_SELECTOR,'body > div > main > section > div > div > div > div.c959f9ac9.cf7e26009 > div > form > div.c028ea628 > button')
-        time.sleep(3)
-        button.click()
-        time.sleep(3)
-        email_chatgpt = driver.find_element(By.CSS_SELECTOR,'#password')
-        time.sleep(3)
-        email_chatgpt.send_keys(PASSWORD)
-        time.sleep(3)
-        button = driver.find_element(By.CSS_SELECTOR,'body > div.oai-wrapper > main > section > div > div > div > form > div.c028ea628 > button')
-        time.sleep(3)
-        button.click()
-        time.sleep(3)
-
 def main():
     #Takes a list of data from instagram and write them into a json.file
     profile_data()
     Sending_prompt_to_chatgpt()
-
 main()
    
 
